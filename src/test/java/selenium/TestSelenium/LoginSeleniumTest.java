63379d1891aa5c7eb44dae9536ab37f439862447
Lucas Bogado <lucasbogadohypo@gmail.com>
2026-05-22 20:03:25 -0300

    feat: adicionando estrutura inicial dos testes selenium + inicio dos testes da tela de cadastro

---
+++ src/test/java/selenium/TestSelenium/LoginSeleniumTest.java
@@ -0,0 +1,32 @@
+package selenium.TestSelenium;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.checkerframework.checker.units.qual.s;
+
+public class LoginSeleniumTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
